8bcfcf1b18d08424999e910b6bc4325d86081157
ppapp <papppali2000@gmail.com>
2026-09-06 01:03:53 +0200

    Initial Commit

---
+++ guiltrip/guiltripApp.swift
@@ -0,0 +1,17 @@
+//
+//  guiltripApp.swift
+//  guiltrip
+//
+//  Created by Personal on 2026. 09. 06..
+//
+
+import SwiftUI
+
+@main
+struct guiltripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ guiltripTests/guiltripTests.swift
@@ -0,0 +1,19 @@
+//
+//  guiltripTests.swift
+//  guiltripTests
+//
+//  Created by Personal on 2026. 09. 06..
+//
+
+import Testing
+@testable import guiltrip
+
+struct guiltripTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
